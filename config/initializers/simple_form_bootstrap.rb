4c2e5ad9300716c219b9be8c1ef18e4ae924ca86
Dima_OHORODNIK <AlvaroMorte8888@gmail.com>
2021-01-26 21:53:24 +0200

    Созадем карточки для нашего поста
    И добавлем когда пост был создан

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -2,20 +2,20 @@
 
 # Please do not make direct changes to this file!
 # This generator is maintained by the community around simple_form-bootstrap:
-# https://github.com/rafaelfranca/simple_form-bootstrap
+# https://github.com/rafaelfrancaform-bootstrap
 # All future development, tests, and organization should happen there.
-# Background history: https://github.com/heartcombo/simple_form/issues/1561
+# Background history: https://github.com/heartcomboform/issues/1561
 
 # Uncomment this and change the path if necessary to include your own
 # components.
-# See https://github.com/heartcombo/simple_form#custom-components
+# See https://github.com/heartcomboform#custom-components
 # to know more about custom components.
 # Dir[Rails.root.join('lib/components/**/*.rb')].each { |f| require f }
 
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   # Default class for buttons
-  config.button_class = 'btn'
+  config.button_class = 'btn btn-success'
 
   # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = 'form-check-label'
@@ -362,7 +362,7 @@ SimpleForm.setup do |config|
 
 
   # Input Group - custom component
-  # see example app and config at https://github.com/rafaelfranca/simple_form-bootstrap
+  # see example app and config at https://github.com/rafaelfrancaform-bootstrap
   # config.wrappers :input_group, tag: 'div', class: 'form-group', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
   #   b.use :html5
   #   b.use :placeholder
